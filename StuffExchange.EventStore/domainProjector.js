bcfc768ebf25135dd2e6b64568f75424ed7cf6a7
Troels Brødsgaard <tr@arbr.dk>
2015-05-22 13:13:56 +0200

    move javascript to js file

---
+++ StuffExchange.EventStore/domainProjector.js
@@ -0,0 +1,15 @@
+﻿// The domainProjection.
+// Remember to run with --run-projections=all
+// and enable emit for the projection
+// and set mode as Continous
+fromAll().when({
+    'GiftAdded': handle,
+    'TitleChanged': handle,
+    'DescriptionUpdated': handle,
+    'ImageAdded': handle,
+    'CommentAdded': handle
+});
+
+function handle(state, ev) {
+    linkTo("domainEvents", ev);
+}
--- StuffExchange.Ports/EventStore.fs
+++ StuffExchange.Ports/EventStore.fs
@@ -21,21 +21,5 @@ let subscribeToDomainEvents eventHandler =
     let streamId = "domainEvents" 
     subscribeToStream streamId eventHandler
 
-(* The domainProjection.
-   Remember to run with --run-projections=all
-   and enable emit for the projection
-   and set mode as Continous
-fromAll().when({
-    'GiftAdded': handle,
-    'TitleChanged': handle,
-    'DescriptionUpdated': handle,
-    'ImageAdded': handle,
-    'CommentAdded': handle
-});
-
-function handle(state, ev) {
-    linkTo("domainEvents", ev);
-}
-*)
 
     
